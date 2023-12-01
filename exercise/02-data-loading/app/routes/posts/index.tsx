8ca7449504706955f506d7439cbd0fc077ed970d
Tamara Temple <tamara@mrets.org>
2023-12-01 00:02:14 -0600

    Complete Exercise 2

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,7 +1,37 @@
+import {Link} from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
+
+const data = {
+  posts: [
+    {
+      slug: "my-first-post",
+      title: "My First Post",
+    },
+    {
+      slug: "90s-mixtape",
+      title: "A Mixtape I Made Just For You",
+    },
+  ],
+};
+
+export const loader = async ({ request }) => {
+  return ({ data })
+}
 export default function Posts() {
+  const {data:{posts}} = useLoaderData();
+
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
